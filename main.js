27eb21d8a171b7fcbd6e890c3ca0886769a12dfb
Jesper Englund <jesper.englund92@gmial.com>
2021-01-08 11:22:03 +0100

    Revert "Added handling of data from the new controller and helper functions"
    This reverts commit 2c3d9cde21db77223d7bcedefda2201436c7de87.

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ enocean.teach({
 enocean.teach({
   'id'  : '00 00 00 2D 9F 39',
   'eep' : 'F6-02-02',
-  'name': 'PullThing'
+  'name': 'Controller 2'
 });
 enocean.teach({
   'id'  : '00 00 01 A4 A2 C2',
@@ -36,7 +36,6 @@ setInterval(myTimer, 500);
 
 var shakes = []
 var prevNrOfShakes = 0
-
 function myTimer() {
   if (shakes.length != prevNrOfShakes){
     db.collection(collection).doc("shaker").update({
@@ -48,81 +47,51 @@ function myTimer() {
   }
 }
 
-function handlePullThingData(message, button){
-  if (message['value'].pressed){
-    if (button == "A0"){
-      db.collection(collection).doc("pause").update({
-          value: true
-      })
-    }
-    else if(button == "B0"){
-      db.collection(collection).doc("newField").update({
-          generate: true
-      })
-    }
-  }
-  else{
-    db.collection(collection).doc("pause").update({
-        value: false
-    })
-    db.collection(collection).doc("newField").update({
-        generate: false
-    })
-  }
-}
-
-function handleJoystickData(message, buttton){
-  var canon = "";
-
-  if (button == "AI"){
-    canon = "leftCanon"
-  }
-  else if(button == "A0"){
-    canon = "topCanon"
-  }
-  else if(button == "B0"){
-    canon = "rightCanon"
-  }
-  else if(button == "BI"){
-    canon = "bottomCanon"
-  }
-
-  if (message['value'].pressed){
-    db.collection(collection).doc(canon).update({
-        shooting: true
-      })
-  }
-  else{
-    let canons = ["leftCanon", "topCanon", "rightCanon", "bottomCanon"]
-    for (var i = 0; i < canons.length; i++) {
-      db.collection(collection).doc(canons[i]).update({
-          shooting: false
-        })
-    }
-  }
-}
-
 // Start to monitor telegrams incoming from the Enocean devices
 enocean.startMonitor().then(() => {
   // Set an event listener for 'data-known' events
   enocean.on('data-known', (telegram) => {
-    console.log(message['device']['name'] + ': ' + message['desc']);
-
     let message = telegram['message'];
+    console.log(message['device']['name'] + ': ' + message['desc']);
     let button = message['value'].button
-    let device = message['device']['name']
 
-    if (device == "Shaker"){
+    if (message['device']['name'] == "Shaker"){
       shakes.push(1)
     }
-    else if(device == "PullThing"){
-      handlePullThingData(message, button)
-    }
     else{
-      handleJoystickData(message, button)
+      var canon = "";
+
+      if (button == "AI"){
+        canon = "leftCanon"
+      }
+      else if(button == "A0"){
+        canon = "topCanon"
+      }
+      else if(button == "B0"){
+        canon = "rightCanon"
+      }
+      else if(button == "BI"){
+        canon = "bottomCanon"
+      }
+
+      if (message['value'].pressed){
+        db.collection(collection).doc(canon).update({
+            shooting: true
+          })
+      }
+      else{
+        let canons = ["leftCanon", "topCanon", "rightCanon", "bottomCanon"]
+        for (var i = 0; i < canons.length; i++) {
+          db.collection(collection).doc(canons[i]).update({
+              shooting: false
+            })
+        }
+      }
     }
+
+
+
   });
-  
 }).catch((error) => {
   console.error(error);
 });
